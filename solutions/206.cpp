91ee8dcabf0821bc8d91a67e8f1d8090a8f2df20
neverbelikeabdullah6075 <muhammadabdullahmateen7@gmail.com>
2026-04-24 13:38:54 +0500

    added 2 new problems

---
+++ solutions/206.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode*current=head;
+        ListNode*prev=nullptr;
+        ListNode*next=nullptr;
+        while(current!=nullptr){
+            next=current->next;
+            current->next=prev;
+            prev=current;
+            current=next;
+        }
+        return prev;
+    }
+};
